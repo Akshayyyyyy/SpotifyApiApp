fe25eca6cf17e22812e07ed5201b835e049bc0f7
Akshay <akshayu718@gmail.com>
2025-07-13 11:39:40 +0530

    write get and listent method

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 import express from 'express';
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Spotify API App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
