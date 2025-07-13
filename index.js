3f8b9d76f368eaf73518f24428810954f8d3ed98
Akshay <akshayu718@gmail.com>
2025-07-13 11:43:07 +0530

    change package type from commanjs to module

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Spotify API App!');
